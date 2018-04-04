ca5339f783412275531683fe97b3910b9ee0714e
Anastasia Lucas <anastasia.lucas@pennmedicine.upenn.edu>
2018-04-04 15:03:43 -0400

    Updated and added file extension option

--- R/apheman.R
+++ R/apheman.R
@@ -4,8 +4,9 @@
 #' Note: There is an issue with dev.off() if using RStudio
 #' Dependencies: ggplot2, gganimate
 #' Suggested: RColorBrewer
-#' @param d data frame, if not plato or plink format, must contain SNP, CHR, POS, pvalue, Frame columns, optional Shape and Color
+#' @param d data frame, if not plato or plink format, must contain PHE, SNP, CHR, POS, pvalue, Frame columns, optional Shape
 #' @param format format of input
+#' @param phegroup optional grouping file for phenotypes, must contain PHE and Group columns
 #' @param line optional pvalue threshold to draw red line at
 #' @param log10 plot -log10() of pvalue column, boolean
 #' @param yaxis label for y-axis, automatically set if log10=TRUE
@@ -24,9 +25,9 @@
 #' @return png image
 #' @export
 #' @examples
-#' agman(d, format, line, log10, yaxis, title, chrcolor1, chrcolor2, groupcolors, file, ext, hgt, wi, res)
+#' apheman(d, format, phegroup, line, log10, yaxis, title, chrcolor1, chrcolor2, groupcolors, file, ext, hgt, wi, res)
 
-agman <- function(d, format="plotman", line, log10=TRUE, yaxis, highlight_snp, highlight_p, highlighter="red", title=NULL, chrcolor1="#AAAAAA", chrcolor2="#4D4D4D", groupcolors, file="agman", ext="html", hgt=7, wi=12, res=300 ){
+apheman <- function(d, format="plotman", phegroup, line, log10=TRUE, yaxis, highlight_snp, highlight_p, highlighter="red", title=NULL, chrcolor1="#AAAAAA", chrcolor2="#4D4D4D", groupcolors, file="apheman", ext="html", hgt=7, wi=12, res=300 ){
   if (!requireNamespace(c("ggplot2"), quietly = TRUE)==TRUE|!requireNamespace(c("gganimate"), quietly = TRUE)==TRUE) {
     stop("Please install ggplot2 and ggiraph to create interactive visualization.", call. = FALSE)
   } else {
@@ -48,7 +49,14 @@ agman <- function(d, format="plotman", line, log10=TRUE, yaxis, highlight_snp, h
 
   #Sort data
   d$CHR <- factor(d$CHR, levels = c("1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20", "21", "22", "X", "Y"))
-  d_order <- d[order(d$CHR, d$POS), ]
+  if(!missing(phegroup)){
+    print("Only phenotypes with grouping information will be plotted")
+    d_phe <- merge(phegroup, d, by="PHE")
+    names(d_phe)[names(d_phe)=="Group"] <- "Color"
+  } else {
+    names(d_phe)[names(d_phe)=="PHE"] <- "Color"
+  }
+  d_order <- d_phe[order(d_phe$CHR, d_phe$POS), ]
   d_order$pos_index <- seq.int(nrow(d_order))
   d_order_sub <- d_order[colnames(d_order) %in% c("SNP", "CHR", "POS", "pvalue", "pos_index")]
 
@@ -68,27 +76,20 @@ agman <- function(d, format="plotman", line, log10=TRUE, yaxis, highlight_snp, h
 
   #Set up colors
   nchrcolors <- nlevels(factor(lims$Color))
-  if("Color" %in% names(d)){
-    if(!missing(groupcolors)){
-      dcols <- c(rep(x=c(chrcolor1, chrcolor2), length.out=nchrcolors, each=1), "#FFFFFF", "#EBEBEB")
-      names(dcols) <-c(levels(factor(lims$Color)), "shade_ffffff", "shade_ebebeb")
-      newcols <- c(dcols, groupcolors)
-    } else {
-      if (!requireNamespace(c("RColorBrewer"), quietly = TRUE)==TRUE) {
-        stop("Please install RColorBrewer to add color attribute.", call. = FALSE)
-      } else {
-        require("RColorBrewer", quietly=TRUE)
-      }
-      ngroupcolors <- nlevels(factor(d_order$Color))
-      getPalette = colorRampPalette(brewer.pal(11, "Spectral"))
-      newcols <- c(rep(x=c(chrcolor1, chrcolor2), length.out=nchrcolors, each=1), getPalette(ngroupcolors), "#FFFFFF", "#EBEBEB")
-      names(newcols) <-c(levels(factor(lims$Color)), levels(factor(d_order$Color)), "shade_ffffff", "shade_ebebeb")
-    }
+  if(!missing(groupcolors)){
+     dcols <- c(rep(x=c(chrcolor1, chrcolor2), length.out=nchrcolors, each=1), "#FFFFFF", "#EBEBEB")
+     names(dcols) <-c(levels(factor(lims$Color)), "shade_ffffff", "shade_ebebeb")
+    newcols <- c(dcols, groupcolors)
   } else {
-    #Color by CHR instead
-    colnames(d_order)[2] <- "Color"
-    newcols <-c(rep(x=c(chrcolor1, chrcolor2), length.out=nchrcolors, each=1), "#FFFFFF", "#EBEBEB")
-    names(newcols) <-c(levels(factor(lims$Color)), "shade_ffffff", "shade_ebebeb")
+    if (!requireNamespace(c("RColorBrewer"), quietly = TRUE)==TRUE) {
+      stop("Please install RColorBrewer to add color attribute.", call. = FALSE)
+    } else {
+       require("RColorBrewer", quietly=TRUE)
+    }
+    ngroupcolors <- nlevels(factor(d_order$Color))
+    getPalette = colorRampPalette(brewer.pal(11, "Spectral"))
+    newcols <- c(rep(x=c(chrcolor1, chrcolor2), length.out=nchrcolors, each=1), getPalette(ngroupcolors), "#FFFFFF", "#EBEBEB")
+    names(newcols) <-c(levels(factor(lims$Color)), levels(factor(d_order$Color)), "shade_ffffff", "shade_ebebeb")
   }
 
   #Info for y-axis
@@ -130,13 +131,8 @@ agman <- function(d, format="plotman", line, log10=TRUE, yaxis, highlight_snp, h
   #}
   p <- p + scale_x_continuous(breaks=lims$av, labels=lims$Color, expand=c(0,0))
   p <- p + geom_rect(data = lims, aes(xmin = posmin-.5, xmax = posmax+.5, ymin = -Inf, ymax = 0, fill=as.factor(Color)), alpha = 1)
-  if("Color" %in% names(d)){
-    #Add legend
-    p <- p + scale_colour_manual(name = "Color", values = newcols) + scale_fill_manual(name = "Color", values = newcols, guides(alpha=FALSE))
-  } else {
-    #Don't
-    p <- p + scale_colour_manual(name = "Color", values = newcols, guides(alpha=FALSE)) + scale_fill_manual(name = "Color", values = newcols, guides(alpha=FALSE))
-  }
+  #Add legend
+  p <- p + scale_colour_manual(name = "Color", values = newcols) + scale_fill_manual(name = "Color", values = newcols, guides(alpha=FALSE))
   p <- p + theme(axis.text.x=element_text(angle=90), panel.grid.minor.x = element_blank(), panel.grid.major.x=element_blank(), axis.title.x=element_blank(), legend.position="bottom", legend.title=element_blank())
   #Highlight if given
   if(!missing(highlight_snp)){
